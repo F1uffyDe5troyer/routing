f96093ea84c3ba89a6518179d8b88875aef6dfda
F1uffyDe5troyer <Dalarnoricardomilago@gmail.com>
2022-01-24 14:02:05 +0200

    alot

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia tempora, omnis ut sequi eum architecto error, ab veritatis necessitatibus voluptatibus alias similique, ipsa officia sint fugit neque? Odio pariatur inventore aspernatur cupiditate ab quos accusamus recusandae voluptatum natus. Molestias ad corrupti totam, consequatur deleniti nesciunt neque impedit reprehenderit harum, ipsam minima provident iste error quis illum debitis esse expedita dolore quas ea quae rerum odio, officiis eaque! Minima iure voluptates aperiam reprehenderit laboriosam provident, veniam rem tempore, beatae nostrum officia dolorem ipsum sequi deserunt saepe maiores odio et? Sunt repudiandae officia explicabo sapiente fuga sit quisquam asperiores consequuntur laudantium similique!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia tempora, omnis ut sequi eum architecto error, ab veritatis necessitatibus voluptatibus alias similique, ipsa officia sint fugit neque? Odio pariatur inventore aspernatur cupiditate ab quos accusamus recusandae voluptatum natus. Molestias ad corrupti totam, consequatur deleniti nesciunt neque impedit reprehenderit harum, ipsam minima provident iste error quis illum debitis esse expedita dolore quas ea quae rerum odio, officiis eaque! Minima iure voluptates aperiam reprehenderit laboriosam provident, veniam rem tempore, beatae nostrum officia dolorem ipsum sequi deserunt saepe maiores odio et? Sunt repudiandae officia explicabo sapiente fuga sit quisquam asperiores consequuntur laudantium similique!</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia tempora, omnis ut sequi eum architecto error, ab veritatis necessitatibus voluptatibus alias similique, ipsa officia sint fugit neque? Odio pariatur inventore aspernatur cupiditate ab quos accusamus recusandae voluptatum natus. Molestias ad corrupti totam, consequatur deleniti nesciunt neque impedit reprehenderit harum, ipsam minima provident iste error quis illum debitis esse expedita dolore quas ea quae rerum odio, officiis eaque! Minima iure voluptates aperiam reprehenderit laboriosam provident, veniam rem tempore, beatae nostrum officia dolorem ipsum sequi deserunt saepe maiores odio et? Sunt repudiandae officia explicabo sapiente fuga sit quisquam asperiores consequuntur laudantium similique!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia tempora, omnis ut sequi eum architecto error, ab veritatis necessitatibus voluptatibus alias similique, ipsa officia sint fugit neque? Odio pariatur inventore aspernatur cupiditate ab quos accusamus recusandae voluptatum natus. Molestias ad corrupti totam, consequatur deleniti nesciunt neque impedit reprehenderit harum, ipsam minima provident iste error quis illum debitis esse expedita dolore quas ea quae rerum odio, officiis eaque! Minima iure voluptates aperiam reprehenderit laboriosam provident, veniam rem tempore, beatae nostrum officia dolorem ipsum sequi deserunt saepe maiores odio et? Sunt repudiandae officia explicabo sapiente fuga sit quisquam asperiores consequuntur laudantium similique!</p>
+   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
